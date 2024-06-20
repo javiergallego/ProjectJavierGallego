95cec907159d9f2b241550cf755d65d26fc36c5d
Javier Gallego <127597564+javiergallego@users.noreply.github.com>
2024-06-20 20:07:16 +0200

    Update Accelerometer.m

--- Pruebas_dsqi/Accelerometer.m
+++ Pruebas_dsqi/Accelerometer.m
@@ -1,4 +1,3 @@
-% Filtrado de las señales
 
 % Listas de archivos
 files_ShirtM = {'C:\Registros\Javi1m.txt', 
@@ -19,33 +18,52 @@ files_ShirtL = {'C:\Registros\Javi1l.txt',
 % Combinamos todas las listas de archivos
 all_files = [files_ShirtM, files_ShirtS, files_ShirtL];
 
+% Inicializar las componentes gravitacionales
+gravityX = 0;
+gravityY = 0;
+gravityZ = 0;
+
+% Constante de filtrado para media exponencial
+alpha = 0.1;
+
 % Procesar cada uno de los archivos
 for i = 1:length(all_files)
     % Obtener el nombre del archivo
     filename = all_files{i};
     
     % Importar los datos del archivo de texto
-    data = readmatrix(filename, 'Delimiter', ' ');  
+    fprintf('Procesando archivo: %s\n', filename);
     
-    numSamples = 100000;
+    % Leer el archivo omitiendo las líneas de cabecera
+    fileID = fopen(filename, 'r');
+    data = textscan(fileID, '%f%f%f%f%f%f', 'HeaderLines', 3, 'Delimiter', '\t');
+    fclose(fileID);
     
+    % Convertir el cell array a matriz
+    data = cell2mat(data);
+
     % Separar los datos en los ejes X, Y, Z
-    adcX = data(:, 1);
-    adcY = data(:, 2);
-    adcZ = data(:, 3);
+    adcX = data(:, 4);
+    adcY = data(:, 5);
+    adcZ = data(:, 6);
 
     % Filtrar las señales
     [accX, accY, accZ] = calibrating(adcX, adcY, adcZ);
 
-    % Calcular la media de las primeras 100000 muestras
-    gravityX = mean(accX(1:numSamples));
-    gravityY = mean(accY(1:numSamples));
-    gravityZ = mean(accZ(1:numSamples));
+    % Actualizar la componente gravitacional usando media exponencial
+    for j = 1:length(accX)
+        gravityX = alpha * accX(j) + (1 - alpha) * gravityX;
+        gravityY = alpha * accY(j) + (1 - alpha) * gravityY;
+        gravityZ = alpha * accZ(j) + (1 - alpha) * gravityZ;
+        
+        % Remover la componente gravitacional de las señales de aceleración
+        accX(j) = accX(j) - gravityX;
+        accY(j) = accY(j) - gravityY;
+        accZ(j) = accZ(j) - gravityZ;
+    end
 
-    % Eliminar la componente gravitacional
-    accX = accX - gravityX;
-    accY = accY - gravityY;
-    accZ = accZ - gravityZ;
+    % Mostrar las componentes gravitacionales finales
+    fprintf('gravityX: %f, gravityY: %f, gravityZ: %f\n', gravityX, gravityY, gravityZ);
 
     % Sumar las aceleraciones en los tres ejes
     sumAcc = accX + accY + accZ;
@@ -60,6 +78,7 @@ for i = 1:length(all_files)
     % Mostrar la potencia calculada para cada archivo
     fprintf('La potencia calculada para %s es: %f\n', filename, power);
 end
+
 
 
 
