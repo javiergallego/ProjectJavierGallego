2a13a1fd437862ea8c2f462032633ef95419b311
Javier Gallego <127597564+javiergallego@users.noreply.github.com>
2024-06-21 20:36:01 +0200

    Update Accelerometer.m
    mejora del codigo para guardar resultados en 12 archivos de texto distintos

--- Pruebas_dsqi/Accelerometer.m
+++ Pruebas_dsqi/Accelerometer.m
@@ -1,4 +1,3 @@
-
 % Listas de archivos
 files_ShirtM = {'C:\Registros\Javi1m.txt', 
     'C:\Registros\Javi2m.txt',
@@ -15,70 +14,87 @@ files_ShirtL = {'C:\Registros\Javi1l.txt',
     'C:\Registros\Javi3l.txt',
     'C:\Registros\Javi4l.txt'};
 
-% Combinamos todas las listas de archivos
-all_files = [files_ShirtM, files_ShirtS, files_ShirtL];
+fileSets = {files_ShirtM, files_ShirtS, files_ShirtL};
 
-% Inicializar las componentes gravitacionales
-gravityX = 0;
-gravityY = 0;
-gravityZ = 0;
 
-% Constante de filtrado para media exponencial
 alpha = 0.1;
 
-% Procesar cada uno de los archivos
-for i = 1:length(all_files)
-    % Obtener el nombre del archivo
-    filename = all_files{i};
-    
-    % Importar los datos del archivo de texto
-    fprintf('Procesando archivo: %s\n', filename);
-    
-    % Leer el archivo omitiendo las líneas de cabecera
-    fileID = fopen(filename, 'r');
-    data = textscan(fileID, '%f%f%f%f%f%f', 'HeaderLines', 3, 'Delimiter', '\t');
-    fclose(fileID);
-    
-    % Convertir el cell array a matriz
-    data = cell2mat(data);
+for setIndex = 1:length(fileSets)
+    currentFiles = fileSets{setIndex};  
 
-    % Separar los datos en los ejes X, Y, Z
-    adcX = data(:, 4);
-    adcY = data(:, 5);
-    adcZ = data(:, 6);
+    for fileIndex = 1:length(currentFiles)
+        currentFile = currentFiles{fileIndex};  
 
-    % Filtrar las señales
-    [accX, accY, accZ] = calibrating(adcX, adcY, adcZ);
+        fileID = fopen(currentFile, 'r');
+        data = textscan(fileID, '%f%f%f%f%f%f', 'HeaderLines', 3, 'Delimiter', '\t');
+        fclose(fileID);
+        data = cell2mat(data);
 
-    % Actualizar la componente gravitacional usando media exponencial
-    for j = 1:length(accX)
-        gravityX = alpha * accX(j) + (1 - alpha) * gravityX;
-        gravityY = alpha * accY(j) + (1 - alpha) * gravityY;
-        gravityZ = alpha * accZ(j) + (1 - alpha) * gravityZ;
-        
-        % Remover la componente gravitacional de las señales de aceleración
-        accX(j) = accX(j) - gravityX;
-        accY(j) = accY(j) - gravityY;
-        accZ(j) = accZ(j) - gravityZ;
+        % Separar los datos en los ejes X, Y, Z
+        adcX = data(:, 4);
+        adcY = data(:, 5);
+        adcZ = data(:, 6);
+
+        % Calibrar las señales
+        [accX, accY, accZ] = calibrating(adcX, adcY, adcZ);
+
+        % Inicializar variables para el cálculo por intervalos
+        numSamples = length(accX);
+        interval = 10000;
+        numIntervals = ceil(numSamples / interval);
+
+        % Inicializar variables para guardar los resultados
+        gravityX_int = zeros(numIntervals, 1);
+        gravityY_int = zeros(numIntervals, 1);
+        gravityZ_int = zeros(numIntervals, 1);
+        power_int = zeros(numIntervals, 1);
+
+        % Procesar por intervalos de 10000 muestras
+        for intervalIdx = 1:numIntervals
+            startIdx = (intervalIdx - 1) * interval + 1;
+            endIdx = min(intervalIdx * interval, numSamples);
+
+            % Actualizar la componente gravitacional usando media exponencial
+            gravityX = 0;
+            gravityY = 0;
+            gravityZ = 0;
+
+            for j = startIdx:endIdx
+                gravityX = alpha * accX(j) + (1 - alpha) * gravityX;
+                gravityY = alpha * accY(j) + (1 - alpha) * gravityY;
+                gravityZ = alpha * accZ(j) + (1 - alpha) * gravityZ;
+
+                
+                accX(j) = accX(j) - gravityX;
+                accY(j) = accY(j) - gravityY;
+                accZ(j) = accZ(j) - gravityZ;
+            end
+
+            % Guardar la componente gravitacional promediada en el intervalo
+            gravityX_int(intervalIdx) = gravityX;
+            gravityY_int(intervalIdx) = gravityY;
+            gravityZ_int(intervalIdx) = gravityZ;
+
+            % Calcular el módulo de los 3 ejes
+            modulo = sqrt(accX(startIdx:endIdx).^2 + accY(startIdx:endIdx).^2 + accZ(startIdx:endIdx).^2);
+
+            % Calcular la potencia promediada en el intervalo
+            numSamplesInterval = length(modulo);
+            power_int(intervalIdx) = sum(modulo) / numSamplesInterval;
+        end
+
+       
+        save_filename = sprintf('results_accel_%d.txt', fileIndex);
+
+        % Guardar los resultados en el archivo
+        fid = fopen(save_filename, 'w');
+        fprintf(fid, 'Interval\tGravityX\tGravityY\tGravityZ\tPower\n');
+        for intervalIdx = 1:numIntervals
+            fprintf(fid, '%d\t%f\t%f\t%f\t%f\n', intervalIdx, abs(gravityX_int(intervalIdx)), abs(gravityY_int(intervalIdx)), abs(gravityZ_int(intervalIdx)), power_int(intervalIdx));
+        end
+        fclose(fid);
     end
-
-    % Mostrar las componentes gravitacionales finales
-    fprintf('gravityX: %f, gravityY: %f, gravityZ: %f\n', gravityX, gravityY, gravityZ);
-
-    % Sumar las aceleraciones en los tres ejes
-    sumAcc = accX + accY + accZ;
-
-    % Calcular la energía como la suma de los valores cuadrados
-    energy = sum(sumAcc.^2);
-
-    % Calcular la potencia dividiendo la energía por el número de muestras
-    numTotalSamples = length(sumAcc);
-    power = energy / numTotalSamples;
-
-    % Mostrar la potencia calculada para cada archivo
-    fprintf('La potencia calculada para %s es: %f\n', filename, power);
 end
-
 
 
 
